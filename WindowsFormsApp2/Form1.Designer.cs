639cbdbb662f51c864b1f6d57fac5abb36d948d5
VIP_Я_VIP VIP_Я_VIP <Roms.iv2003@yandex.ru>
2022-12-26 19:35:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imageTovar = new System.Windows.Forms.DataGridViewImageColumn();
+            this.nameTovar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionTovar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.manufacture = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amountStore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.imageTovar,
+            this.nameTovar,
+            this.descriptionTovar,
+            this.manufacture,
+            this.price,
+            this.amountStore});
+            this.dataGridView1.Location = new System.Drawing.Point(39, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 100;
+            this.dataGridView1.Size = new System.Drawing.Size(1066, 513);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // imageTovar
+            // 
+            this.imageTovar.HeaderText = "Фото товара";
+            this.imageTovar.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.imageTovar.Name = "imageTovar";
+            this.imageTovar.Width = 200;
+            // 
+            // nameTovar
+            // 
+            this.nameTovar.HeaderText = "Наименование товара";
+            this.nameTovar.Name = "nameTovar";
+            this.nameTovar.Width = 150;
+            // 
+            // descriptionTovar
+            // 
+            this.descriptionTovar.HeaderText = "Описание товара";
+            this.descriptionTovar.Name = "descriptionTovar";
+            this.descriptionTovar.Width = 500;
+            // 
+            // manufacture
+            // 
+            this.manufacture.HeaderText = "Производитель";
+            this.manufacture.Name = "manufacture";
+            // 
+            // price
+            // 
+            this.price.HeaderText = "Цена";
+            this.price.Name = "price";
+            this.price.Width = 75;
+            // 
+            // amountStore
+            // 
+            this.amountStore.HeaderText = "Количество на складе";
+            this.amountStore.Name = "amountStore";
+            this.amountStore.Width = 50;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(505, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Без сортировки",
+            "По убыванию",
+            "По возрастанию"});
+            this.comboBox1.Location = new System.Drawing.Point(636, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(456, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Все производители"});
+            this.comboBox2.Location = new System.Drawing.Point(636, 51);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(456, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(458, 605);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(498, 605);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(9, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "|";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 605);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 638);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewImageColumn imageTovar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameTovar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionTovar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn manufacture;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amountStore;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BDLoad();
+            LoadManufacture();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void BDLoad()
+        {
+            dataGridView1.Rows.Clear();
+            using (EXAM11Entities db = new EXAM11Entities())
+            {
+                var DataTovar = db.TOVARS;
+                label1.Text = DataTovar.Count().ToString();
+                label3.Text = DataTovar.Count().ToString();
+                foreach(var tovar in DataTovar)
+                {
+                    DataGridViewImageColumn img = new DataGridViewImageColumn();
+                    try
+                    {
+                        Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                        img.Image = image;
+                    }
+                    catch
+                    {
+                        Image image = new Bitmap($"../../Images/picture.png");
+                        img.Image = image;
+                    }
+                    dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image,db.NAMETOVAR.Where(n => n.IDNAMETOVAR == tovar.IDNAMETOVAR).FirstOrDefault().NAMETOVAR1,tovar.DESCRIPTIONS, db.MANUFACTURE.Where(n => n.IDMANUFACTURE == tovar.IDMANUFACTURE).FirstOrDefault().NAMEMANUFACTURE, tovar.PRICE,tovar.AMOUNTSTORE);
+                    if(tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count-1].DefaultCellStyle.BackColor = Color.Gray; }
+                }
+            }
+        }
+
+        private void LoadManufacture()
+        {
+            using (EXAM11Entities db = new EXAM11Entities())
+            {
+                var DataManufacture = db.MANUFACTURE;
+                foreach(var manufacture in DataManufacture)
+                {
+                    comboBox2.Items.Add(manufacture.NAMEMANUFACTURE);
+                }
+            }
+        }
+
+        private void SearchLoad()
+        {
+            dataGridView1.Rows.Clear();
+            using (EXAM11Entities db = new EXAM11Entities())
+            {
+                if ((comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 0 && comboBox2.SelectedIndex != -1) || (comboBox1.SelectedIndex == -1 && comboBox2.SelectedIndex == -1)) 
+                {
+                    var DataTovar = db.VIEWSEARCH(textBox1.Text); 
+                    foreach (var tovar in DataTovar)
+                    {
+                        DataGridViewImageColumn img = new DataGridViewImageColumn();
+                        try
+                        {
+                            Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                            img.Image = image;
+                        }
+                        catch
+                        {
+                            Image image = new Bitmap($"../../Images/picture.png");
+                            img.Image = image;
+                        }
+                        dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image, tovar.NAMETOVAR, tovar.DESCRIPTIONS, tovar.NAMEMANUFACTURE, tovar.PRICE, tovar.AMOUNTSTORE);
+                        if (tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count - 1].DefaultCellStyle.BackColor = Color.Gray; }
+                    }
+                    label1.Text = DataTovar.Count().ToString();
+                }
+                if ((comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 0 && comboBox2.SelectedIndex != -1) || (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == -1))
+                {
+                    var DataTovar = db.VIEWSEARCH(textBox1.Text).OrderByDescending(t => t.PRICE);
+                    foreach (var tovar in DataTovar)
+                    {
+                        DataGridViewImageColumn img = new DataGridViewImageColumn();
+                        try
+                        {
+                            Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                            img.Image = image;
+                        }
+                        catch
+                        {
+                            Image image = new Bitmap($"../../Images/picture.png");
+                            img.Image = image;
+                        }
+                        dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image, tovar.NAMETOVAR, tovar.DESCRIPTIONS, tovar.NAMEMANUFACTURE, tovar.PRICE, tovar.AMOUNTSTORE);
+                        if (tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count - 1].DefaultCellStyle.BackColor = Color.Gray; }
+                    }
+                    label1.Text = DataTovar.Count().ToString();
+                }
+                if ((comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 0 && comboBox2.SelectedIndex != -1) || (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == -1))
+                {
+                    var DataTovar = db.VIEWSEARCH(textBox1.Text).OrderBy(t => t.PRICE);
+                    foreach (var tovar in DataTovar)
+                    {
+                        DataGridViewImageColumn img = new DataGridViewImageColumn();
+                        try
+                        {
+                            Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                            img.Image = image;
+                        }
+                        catch
+                        {
+                            Image image = new Bitmap($"../../Images/picture.png");
+                            img.Image = image;
+                        }
+                        dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image, tovar.NAMETOVAR, tovar.DESCRIPTIONS, tovar.NAMEMANUFACTURE, tovar.PRICE, tovar.AMOUNTSTORE);
+                        if (tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count - 1].DefaultCellStyle.BackColor = Color.Gray; }
+                    }
+                    label1.Text = DataTovar.Count().ToString();
+                }
+                if ((comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex != 0 && comboBox2.SelectedIndex != -1) || (comboBox1.SelectedIndex == -1 && comboBox2.SelectedIndex != 0 && comboBox2.SelectedIndex != -1))
+                {
+                    var DataTovar = db.VIEWSEARCH(textBox1.Text).Where(t => t.NAMEMANUFACTURE == comboBox2.SelectedItem.ToString());
+                    foreach (var tovar in DataTovar)
+                    {
+                        DataGridViewImageColumn img = new DataGridViewImageColumn();
+                        try
+                        {
+                            Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                            img.Image = image;
+                        }
+                        catch
+                        {
+                            Image image = new Bitmap($"../../Images/picture.png");
+                            img.Image = image;
+                        }
+                        dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image, tovar.NAMETOVAR, tovar.DESCRIPTIONS, tovar.NAMEMANUFACTURE, tovar.PRICE, tovar.AMOUNTSTORE);
+                        if (tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count - 1].DefaultCellStyle.BackColor = Color.Gray; }
+                    }
+                    label1.Text = DataTovar.Count().ToString();
+                }
+                if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex != 0 && comboBox2.SelectedIndex != -1)
+                {
+                    var DataTovar = db.VIEWSEARCH(textBox1.Text).OrderByDescending(t => t.PRICE).Where(t => t.NAMEMANUFACTURE == comboBox2.SelectedItem.ToString());
+                    foreach (var tovar in DataTovar)
+                    {
+                        DataGridViewImageColumn img = new DataGridViewImageColumn();
+                        try
+                        {
+                            Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                            img.Image = image;
+                        }
+                        catch
+                        {
+                            Image image = new Bitmap($"../../Images/picture.png");
+                            img.Image = image;
+                        }
+                        dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image, tovar.NAMETOVAR, tovar.DESCRIPTIONS, tovar.NAMEMANUFACTURE, tovar.PRICE, tovar.AMOUNTSTORE);
+                        if (tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count - 1].DefaultCellStyle.BackColor = Color.Gray; }
+                    }
+                    label1.Text = DataTovar.Count().ToString();
+                }
+                if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex != 0 && comboBox2.SelectedIndex != -1)
+                {
+                    var DataTovar = db.VIEWSEARCH(textBox1.Text).OrderBy(t => t.PRICE).Where(t => t.NAMEMANUFACTURE == comboBox2.SelectedItem.ToString());
+                    foreach (var tovar in DataTovar)
+                    {
+                        DataGridViewImageColumn img = new DataGridViewImageColumn();
+                        try
+                        {
+                            Image image = new Bitmap($"../../Images/{tovar.IMAGENAME}");
+                            img.Image = image;
+                        }
+                        catch
+                        {
+                            Image image = new Bitmap($"../../Images/picture.png");
+                            img.Image = image;
+                        }
+                        dataGridView1.Rows.Add(tovar.IDTOVARS, img.Image, tovar.NAMETOVAR, tovar.DESCRIPTIONS, tovar.NAMEMANUFACTURE, tovar.PRICE, tovar.AMOUNTSTORE);
+                        if (tovar.AMOUNTSTORE <= 0) { dataGridView1.Rows[dataGridView1.Rows.Count - 1].DefaultCellStyle.BackColor = Color.Gray; }
+                    }
+                    label1.Text = DataTovar.Count().ToString();
+                }
+            }
+        }
+       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SearchLoad();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SearchLoad();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SearchLoad();
+        }
+    }
+}
